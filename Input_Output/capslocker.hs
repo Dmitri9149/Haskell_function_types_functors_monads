c7f37f5d5aa3711a076e7d6e9871d401302dec47
dmitri9149 <dmitri.as.name@gmail.com>
2020-08-21 17:34:11 +0300

    capsloker function

--- Input_Output/capslocker.hs
+++ Input_Output/capslocker.hs
@@ -1,6 +1,6 @@
 import Data.Char
 
--- thefunction prompted the user for a line, returned it to him in CAPSLOCK 
+-- the function prompted the user for a line, returned it to him in CAPSLOCK 
 --  and then did that all over again, indefinitely
 main = do 
     contents <- getContents
